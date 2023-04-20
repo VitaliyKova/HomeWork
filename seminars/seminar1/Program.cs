56f3fbcb239d15cd3170ec05e394f8e45d25f720
«VITALIY» <dr.kovalyk2017@yandex.ru>
2023-04-20 21:38:40 +0300

    Прошел первый семинар по С#

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"You input {num} numbers");
+
+// Console.WriteLine("Input number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Quad of {num} = {num * num}");
+
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2 * num2)
+// {
+//     Console.WriteLine($"{num1} является квадратом {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"{num1} не является квадратом {num2}");
+// }
+
+// Console.WriteLine("Input N");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if(n < 0) Console.WriteLine("No");
+// int count = n*(-1);
+// while(count != n + 1)
+// {
+//     Console.Write(count + " ");
+//     count++;
+// }
+
+Console.WriteLine("Input thre-digit number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10);
