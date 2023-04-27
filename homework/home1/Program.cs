d6776d22218ba4e42ba1f50ee6c86c9cdd94877c
«VITALIY» <dr.kovalyk2017@yandex.ru>
2023-04-27 18:12:17 +0300

    HomeWork1

---
+++ homework/home1/Program.cs
@@ -0,0 +1,69 @@
+﻿/* 
+Задача 1: Напишите программу, 
+которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее
+*/
+// Console.WriteLine("Введите 1 число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"Наибольшее число - {num1}");
+//     Console.WriteLine($"Наименьшее число - {num2}");
+// }
+// else 
+// {
+//     Console.WriteLine($"Наибольшее число - {num2}");
+//     Console.WriteLine($"Наименьшее число - {num1}");
+// }
+/*
+Задача 2: Напишите программу, которая принимает на вход 
+три числа и выдаёт максимальное из этих чисел.
+*/
+// Console.WriteLine("Введите 1-е число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2-е число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 3-е число");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if(num2 > max)
+// {
+//     max = num2;
+// }
+// if(num3 > max)
+// {
+//     max = num3;
+// }
+// Console.WriteLine($"Максимальное число - {max}");
+
+/*
+Задача 3: Напишите программу, которая на вход принимает число 
+и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+*/
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num % 2 == 0)
+// {
+//     Console.WriteLine($"Число {num} четное");
+// }
+// else Console.WriteLine($"Число {num} нечетное");
+
+/*
+Задача 4: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+// Console.WriteLine("Введите число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while(count <= n)
+// {
+//     if(count % 2 == 0)
+//     {
+//         Console.Write($"{count} ");
+//     }
+//     count++;
+// }
