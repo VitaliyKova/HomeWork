e12afea4896e86d0b27df461dda7dee666bc6ac3
«VITALIY» <dr.kovalyk2017@yandex.ru>
2023-04-27 16:43:41 +0300

    second lection

---
+++ seminars/seminar2/Program.cs
@@ -0,0 +1,25 @@
+﻿// int Maxdigit (int randomnum)
+// {
+//     int dec = randomnum / 10;
+//     int ed = randomnum % 10;
+//     if(dec > ed) return dec;
+//     else return ed;
+// }
+
+// int num = new Random().Next(10, 99 + 1);
+
+// int result = Maxdigit(num);
+// Console.WriteLine($"{num} -> {result}");
+
+// bool Kratnost (int num1, int num2)
+// {
+//     if(num1 % num2 == 0) return true;
+//     else return false;
+// }
+// Console.WriteLine("Input first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{num1}, {num2} -> {Kratnost(num1, num2)}");
+
