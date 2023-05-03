bf644259baccb58961d58fb37539952ebdab9d12
«VITALIY» <dr.kovalyk2017@yandex.ru>
2023-05-03 22:38:32 +0300

    Первая задача в 3 дз

---
+++ homework/home3/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+// bool pal(int num) 
+// {
+//     if(num / 10000 == num % 10)
+//     {
+//         if(num / 1000 == (num / 10) % 10);
+//         {
+//              return true;
+//         }
+//     } 
+//     else return false;
+// }
+// Console.WriteLine("Введите пятизначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num < 100000 && num > 9999)
+// {
+//     bool finish = pal(num);
+//     if(finish == true)
+//     {
+//         Console.WriteLine($"Число {num} является палиндромом");
+//     }
+//     else Console.WriteLine($"Число {num} не палиндром");    
+// }
+// else Console.WriteLine($"Введено не пятизначное число!");
+   
