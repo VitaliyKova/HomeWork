6b4a80142815f18bc9e25c4dfa30beeaac49429a
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-05-24 00:34:31 +0300

    Lection5

---
+++ lection/lection5/Program.cs
@@ -0,0 +1,44 @@
+﻿// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+// }
+// }
+
+
+
+
+
+// void FillArray (int [,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for ( int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i,j] = new Random().Next(1,10);
+//         }
+//     }
+// }
+
+// int [,] matrix = new int[3,4];
+// PrintArray(matrix);
+// Console.WriteLine();
+// FillArray(matrix);
+// PrintArray(matrix);
+
+
+// РЕКУРСИЯ
+// int Factorial (int n)
+// {
+//     if(n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+// Console.WriteLine(Factorial(3));
+
+// ФИБОНАЧИ
+
